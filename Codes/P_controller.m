b9c866c9ecb8b341f4a831b897ac72a3d25f6dba
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-05-09 11:09:08 +0300

    P controller code

---
+++ Codes/P_controller.m
@@ -0,0 +1,11 @@
+m = 1;
+b = 0.5;
+k = 0.25;
+transfer_fnc = tf(1, [m,b,k]);
+kp = 6.71926735265958;
+ki = 0;
+kd = 0;
+c = pid(kp, ki, kd);
+T = feedback(c*transfer_fnc, 1);
+step(T, 9)
+info = stepinfo(T)
