1d5e68a110d6b16f2035921fe6b86db78daa827a
Ahmed Shalash <ahmed02shalash@gmail.com>
2026-05-09 11:18:10 +0300

    PI controller with its results

---
+++ Codes/PI_controller.m
@@ -0,0 +1,11 @@
+m = 1;
+b = 0.5;
+k = 0.25;
+transfer_fnc = tf(1, [m,b,k]);
+kp = 6.71926735265958;
+ki = 1.97608543136126;
+kd = 0;
+c = pid(kp, ki, kd);
+T = feedback(c*transfer_fnc, 1);
+step(3*T, 9)
+info = stepinfo(T)
